db8bd77eaca6226e290177a54d7f5bfc8abb9126
Gert Cuykens <gert.cuykens@gmail.com>
2014-10-02 23:02:00 +0200

    form

--- app.go
+++ app.go
@@ -31,12 +31,24 @@ func submit(w http.ResponseWriter, r *http.Request) {
     }
     if code := r.FormValue("code"); code != "12345" {
         c.Errorf("Wrong code: %v", code)
-        fmt.Fprintf(w,"{\"status\":\"Code invalid! Try again\", \"code\":501}")
+        http.Error(w, "{\"status\":\"Code invalid! Try again\", \"code\":401}", http.StatusUnauthorized)
     }else if err := mail.Send(c, msg); err != nil {
         c.Errorf("Couldn't send email: %v", err)
-        fmt.Fprint(w, "{\"status\":\"Mail NOT send! Error\", \"code\":500}")
+        http.Error(w, "{\"status\":\"Mail NOT send! Error\", \"code\":500}", http.StatusInternalServerError)
     }else{
         c.Infof("Mail send:\n %v", message)
         fmt.Fprint(w, "{\"status\":\"Mail send\", \"code\":200}")
+    }
+}
+
+func addDefaultHeaders(fn http.HandlerFunc) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        if origin := r.Header.Get("Origin"); origin != "" {
+            w.Header().Set("Access-Control-Allow-Origin", origin)
+        }
+        //w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+        //w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token")
+        //w.Header().Set("Access-Control-Allow-Credentials", "true")
+        fn(w, r)
     }
 }
