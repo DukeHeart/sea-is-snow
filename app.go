29d481166053cb6bc56443a455027f52bcf6f53f
Gert Cuykens <gert.cuykens@gmail.com>
2014-10-02 04:38:38 +0200

    submit

--- app.go
+++ app.go
@@ -16,25 +16,27 @@ func submit(w http.ResponseWriter, r *http.Request) {
     c := appengine.NewContext(r)
     name := r.FormValue("name")
     email := r.FormValue("email")
-    subject := r.FormValue("subject")
-    message := r.FormValue("message")
+    message := "<html><body><h3>Contact:</h3><p>Phone: " + r.FormValue("phone") +"<br/>Message: "+ r.FormValue("message") +"</p><h3>Order:</h3><p>"+ r.FormValue("order") + "</p></body></html>"
     msg := &mail.Message{
             Sender:  name + " <submit@sea-is-snow.appspotmail.com>",
             To:      []string{"gert.cuykens.2@gmail.com"},
             ReplyTo: email,
-            Subject: subject,
-            Body:    message,
+            Subject: "seaissnow.nl",
+            //Body:    message,
+            HTMLBody: message,
             Headers: netMail.Header{
+                //"Content-Type": []string{"text/html; charset=UTF-8"},
                 "On-Behalf-Of": []string{email},
             },
     }
-    if code := r.FormValue("code"); code != "1234" {
+    if code := r.FormValue("code"); code != "12345" {
         c.Errorf("Wrong code: %v", code)
-        fmt.Fprintf(w, fmt.Sprintf("{\"status\":\"Code invalid! %s Try again\"}", code))
+        fmt.Fprintf(w,"{\"status\":\"Code invalid! Try again\", \"code\":501}")
     }else if err := mail.Send(c, msg); err != nil {
         c.Errorf("Couldn't send email: %v", err)
-        fmt.Fprint(w, "{\"status\":\"Mail NOT send! Error\"}")
+        fmt.Fprint(w, "{\"status\":\"Mail NOT send! Error\", \"code\":500}")
     }else{
-        fmt.Fprint(w, "{\"status\":\"Mail send.\"}")
+        c.Infof("Mail send:\n %v", message)
+        fmt.Fprint(w, "{\"status\":\"Mail send\", \"code\":200}")
     }
 }
